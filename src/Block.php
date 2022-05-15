f3f6ae15e41a554fe73be5eea7fc2aeb0934753e
BlusterySasha <blusterysasha@vk.com>
2022-05-15 23:12:38 +0400

    Update?

--- src/Block.php
+++ src/Block.php
@@ -1,92 +1,95 @@
 <?php
+
+declare(strict_types=1);
+
 namespace XackiGiFF\TapToDoNew;
 
-use pocketmine\world\Position;
+use JetBrains\PhpStorm\ArrayShape;
 use pocketmine\player\Player;
+use pocketmine\world\Position;
 
-class Block{
-    /** @var  Command[] */
-    private $commands;
-    /** @var  Position */
-    private $position;
-    /** @var mixed */
-    private $name;
-    /** @var Main  */
-    private $plugin;
-    public $id;
-    public function __construct(Position $position, array $commands, Main $main, $id, $name = false){
-        $this->position = $position;
-        $this->commands = [];
-        $this->plugin = $main;
-        $this->name = $name;
-        $this->id = $id;
+class Block {
+	public mixed $id;
+	/** @var Command[] */
+	private array $commands;
+	private Position $position;
+	private string|bool $name;
+	private Main $plugin;
 
-        $this->addCommands($commands);
-    }
-    public function addCommands($cmds){
-        if(!is_array($cmds)){
-            $cmds = [$cmds];
-        }
-        $this->commands = array_values($this->commands);
-        foreach ($cmds as $c) {
-            $this->commands[] = new Command($c, $this->plugin);
-        }
-        $this->plugin->saveBlock($this);
-    }
-    public function addCommand($cmd){
-        $this->addCommands([$cmd]);
-    }
-    public function deleteCommand($cmd){
-        $ret = false;
-        $i = 0;
-        $this->commands = array_values($this->commands);
-        for($i = count($this->commands)-1; $i >= 0; $i--){
-            if($this->commands[$i]->getOriginalCommand() === $cmd || $this->commands[$i]->getCompiledCommand() === $cmd){
-                unset($this->commands[$i]);
-                $ret = true;
-            }
-        }
-        if($ret){
-            $this->plugin->saveBlock($this);
-        }
-        return $ret;
-    }
-    public function executeCommands(Player $player){
-        foreach($this->commands as $command){
-            $command->execute($player);
-        }
-    }
-    public function setName($name){
-        $this->name = $name;
-    }
-    public function getCommands(){
-        $out = [];
-        foreach($this->commands as $command) $out[] = $command->getOriginalCommand();
-        return $out;
-    }
-    public function getName(){
-        return $this->name;
-    }
+	public function __construct(Position $position, array $commands, Main $main, mixed $id, $name = FALSE) {
+		$this->position = $position;
+		$this->commands = [];
+		$this->plugin = $main;
+		$this->name = $name;
+		$this->id = $id;
 
-    /**
-     * @return Position
-     * @deprecated
-     */
-    public function getPos(){
-        return $this->position;
-    }
-    public function getPosition(){
-        return $this->position;
-    }
-    public function toArray(){
-        $arr = [
-            'x' => $this->getPosition()->getX(),
-            'y' => $this->getPosition()->getY(),
-            'z' => $this->getPosition()->getZ(),
-            'world' => $this->getPosition()->getWorld()->getDisplayName(),
-            'commands' => $this->getCommands()
-        ];
-        if($this->name !== false) $arr["name"] = $this->name;
-        return $arr;
-    }
+		$this->addCommands($commands);
+	}
+
+	public function addCommands($cmds) : void {
+		if (!is_array($cmds)) {
+			$cmds = [$cmds];
+		}
+		$this->commands = array_values($this->commands);
+		foreach ($cmds as $c) {
+			$this->commands[] = new Command($c, $this->plugin);
+		}
+		$this->plugin->saveBlock($this);
+	}
+
+	public function addCommand($cmd) : void {
+		$this->addCommands([$cmd]);
+	}
+
+	public function deleteCommand($cmd) : bool {
+		$ret = FALSE;
+		$this->commands = array_values($this->commands);
+		for ($i = count($this->commands) - 1; $i >= 0; $i--) {
+			if ($this->commands[$i]->getOriginalCommand() === $cmd || $this->commands[$i]->getCompiledCommand() === $cmd) {
+				unset($this->commands[$i]);
+				$ret = TRUE;
+			}
+		}
+		if ($ret) {
+			$this->plugin->saveBlock($this);
+		}
+		return $ret;
+	}
+
+	public function executeCommands(Player $player) : void {
+		foreach ($this->commands as $command) {
+			$command->execute($player);
+		}
+	}
+
+	public function getCommands() : array {
+		$out = [];
+		foreach ($this->commands as $command) $out[] = $command->getOriginalCommand();
+		return $out;
+	}
+
+	public function getName() {
+		return $this->name;
+	}
+
+	public function setName($name) : void {
+		$this->name = $name;
+	}
+
+	public function getPosition() : Position {
+		return $this->position;
+	}
+
+	#[ArrayShape(['x' => "float|int", 'y' => "float|int", 'z' => "float|int", 'world' => "string", 'commands' => "array", "name" => "bool|mixed|string"])]
+	public function toArray() : array {
+		$arr = [
+			'x' => $this->getPosition()->getX(),
+			'y' => $this->getPosition()->getY(),
+			'z' => $this->getPosition()->getZ(),
+			'world' => $this->getPosition()->getWorld()->getDisplayName(),
+			'commands' => $this->getCommands()
+		];
+		if ($this->name !== FALSE) $arr["name"] = $this->name;
+		return $arr;
+	}
 }
